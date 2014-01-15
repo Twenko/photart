1cd9bfcef6c3042a121a41b29b940e058cfb1d2b
Khin <nicolas.benkemoun@gmail.com>
2014-01-15 21:09:54 +0100

    full base

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Photart::Application.config.secret_token = 'afd56d0632fc198ecff8c6255467cb244b4b7f83f27c0a3167c6f8cb85cecc5b711b1bd49862fcd175aa4abf94d2626c9b7b3478bf09c44f192931bd6d83b379'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Photart::Application.config.session_store :cookie_store, :key => '_photart_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Photart::Application.config.session_store :active_record_store
